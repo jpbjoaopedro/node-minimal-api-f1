daf39d74eb9e20ac4cc3a2efc4537f4d22d3c984
Joao Pedro Bandeira <jpbandeiradossantos@gmail.com>
2025-05-14 13:05:30 -0300

    refactor: improve route handlers and add driver retrieval by ID

--- src/server.ts
+++ src/server.ts
@@ -4,16 +4,35 @@ import { drivers } from "./repository/drivers";
 
 const server = fastify({ logger: true });
 
-server.get("/teams", async (request, response) => {
-  response.type("application/json").code(200);
+server.get("/teams", async (req, res) => {
+  res.type("application/json").code(200);
 
   return { teams };
 });
 
-server.get("/drivers", async (request, response) => {
-    response.type("application/json").code(200);
+server.get("/drivers", async (req, res) => {
+    res.type("application/json").code(200);
 
     return { drivers }
+});
+
+interface DriverParams {
+    id: string
+}
+
+server.get<{Params: DriverParams}>("/drivers/:id", async (req, res) => {
+    const idParam = parseInt(req.params.id);
+    const driver = drivers.find((d) => d.id === idParam )
+
+    if(!driver) {
+        res.type("application/json").code(204)
+        return { message: "Driver Not Found" }
+    } else {
+        res.type("application/json").code(200)
+        return { driver }
+    }
+
+
 });
 
 const PORT = process.env.PORT
